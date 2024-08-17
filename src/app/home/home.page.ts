7b2da71bac8ff2ee7b2f0fa433d7e705854da3fa
Muhammad Syazani <syazanifaudzi@gmail.com>
2024-08-17 10:17:21 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  imageUrl: string | undefined;
+
   constructor() {}
 
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Get the image as a file URI
+      source: CameraSource.Camera, // Select the camera as the source
+    });
+ 
+    this.imageUrl = image.webPath; // Set the image URL for display
+  }
+ 
 }
